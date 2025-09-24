a98b87762e05297eadaa45d27c09a5aba5dbeb73
Adedara Samuel <adedarasapokgmail.com>
2025-09-24 11:35:26 +0100

    Fix: add @google-cloud/tasks dependency

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   // transpilePackages: ['@firebase/app', '@firebase/auth', '@firebase/firestore', '@firebase/storage'],
   reactStrictMode: false,
   devIndicators: false
+
+  webpack: (config) => {
+    config.externals.push('@google-cloud/tasks');
+    return config;
+  },
+
   /* config options here */
 };
 
