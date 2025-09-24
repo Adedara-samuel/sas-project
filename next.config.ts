0eabe7216751da9396b438a36abcaaea0d409014
Adedara Samuel <adedarasapokgmail.com>
2025-09-24 11:36:12 +0100

    fix on next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // transpilePackages: ['@firebase/app', '@firebase/auth', '@firebase/firestore', '@firebase/storage'],
   reactStrictMode: false,
-  devIndicators: false
+  devIndicators: false,
 
   webpack: (config) => {
     config.externals.push('@google-cloud/tasks');
